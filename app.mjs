e5fe115f5ff8f3d74de54d5b4d7e7e7dacd94916
ikuradon <hawaiianphoto@geekhost.net>
2023-06-23 15:06:40 +0900

    Add forecast information

--- app.mjs
+++ app.mjs
@@ -545,6 +545,57 @@ const messageWeatherForecast = async (location) => {
     console.log(class10sCode);
     const officesCode = Object.entries(areaData.class10s).filter(record => (record[0] === class10sCode))[0][1].parent;
     console.log(officesCode);
+
+    const forecastUrl = "https://www.jma.go.jp/bosai/forecast/data/forecast/";
+    const response = await axios.get(`${forecastUrl}${officesCode}.json`);
+
+    let arrayId;
+    for (let i = 0; i < response.data[0].timeSeries[0].areas.length; i++) {
+      if (response.data[0].timeSeries[0].areas[i].area.code === class10sCode) {
+        arrayId = i;
+        break;
+      }
+    }
+
+    const forecastsShort = response.data[0].timeSeries;
+
+    const forecastsShortTemps = forecastsShort[2].areas[arrayId].temps;
+    if (9 <= new Date().getHours() && new Date().getHours() < 18)
+      forecastsShortTemps.splice(1, 1);
+    const forecastsShortTempsLength = forecastsShortTemps.length;
+    for (let i = 0; i < 4 - forecastsShortTempsLength; i++)
+      forecastsShortTemps.unshift("--");
+
+    const forecastShortPops = forecastsShort[1].areas[arrayId].pops.map(element => element.padStart(3, " ") + "%");
+    const forecastShortPopsLength = forecastShortPops.length
+    for (let i = 0; i < 8 - forecastShortPopsLength; i++) {
+      forecastShortPops.unshift("----");
+    }
+
+    const forecastsLong = response.data[1].timeSeries;
+    const timeDefinesLong = forecastsLong[0].timeDefines;
+    const forecastLongAreas = [];
+    for (let i = 0; i < forecastsLong[0].areas.length; i++) {
+      forecastLongAreas[i] = {
+        weather: forecastsLong[0].areas[i],
+        amedas: forecastsLong[1].areas[i],
+      }
+    }
+
+    const area = forecastLongAreas[arrayId];
+    message += `${format(new Date(forecastsShort[0].timeDefines[0]), "yyyy-MM-dd")} ${forecastsShortTemps[0]}/${forecastsShortTemps[1]} ${forecastsShort[0].areas[arrayId].weathers[0]}\n`;
+    message += `降水確率: ${[...forecastShortPops].slice(0, 4).join(" / ")}\n`;
+    message += `${format(new Date(forecastsShort[0].timeDefines[1]), "yyyy-MM-dd")} ${forecastsShortTemps[2]}/${forecastsShortTemps[3]} ${forecastsShort[0].areas[arrayId].weathers[1]}\n`;
+    message += `降水確率: ${[...forecastShortPops].slice(4).join(" / ")}\n`;
+
+    message += "---------------\n";
+
+    for (let i = 2; i < timeDefinesLong.length; i++) {
+      message += `${format(new Date(timeDefinesLong[i]), "yyyy-MM-dd")} (${area.weather.reliabilities[i]}) ${area.amedas.tempsMin[i]}/${area.amedas.tempsMax[i]} ${area.weather.pops[i]}% ${CONST.TELOPS[area.weather.weatherCodes[i]][3]}\n`;
+    }
+
+    message += "---------------\n";
+
     const forecastData = (await axios.get(`https://www.jma.go.jp/bosai/forecast/data/overview_forecast/${officesCode}.json`)).data;
     console.log(forecastData.text);
     message += forecastData.text;
